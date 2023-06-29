2e91e84eb4b44135dd1701badfd6320e8f045341
Hitesh Daga <hiteshdaga0@gmail.com>
2023-06-30 02:03:30 +0530

    Basics of React

---
+++ REACT-APPLICATION/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Temp from "./components/weather/temp";
+//import Restaurant from "./components/Basics/Restaurant";
+//import UseState from "./components/Hooks/useState";
+// import UseEffect from "./components/Hooks/useEffect";
+// import UseReducer from "./components/Hooks/useReducer";
+//import Todo from "./components/todoreact/todo.js";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
